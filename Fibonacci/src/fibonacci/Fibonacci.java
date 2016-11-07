80e64d5cb763ca749cd7b9bcb1f99dc1115d0b49
Brayan Jimenez <brayan-andriu@hotmail.com>
2016-11-06 22:06:09 -0500

    segunda version con recursividad

--- Fibonacci/src/fibonacci/Fibonacci.java
+++ Fibonacci/src/fibonacci/Fibonacci.java
@@ -16,25 +16,21 @@ public class Fibonacci {
     /**
      * @param args the command line arguments
      */
+    
+    
+   public static int FibonacciRecursivo(int n){
+         if(n < 2)
+            return 1;
+         else
+            return FibonacciRecursivo(n-1) + FibonacciRecursivo(n-2);
+      }
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-        int numero,fibo1,fibo2,i;
-        do{
+        int numero;
             System.out.print("Introduce numero mayor que 1: ");
             numero = sc.nextInt();
-        }while(numero<=1);
-        System.out.println("Los " + numero + " primeros términos de la serie de Fibonacci son:"); 
-
-        fibo1=1;
-        fibo2=1; 
-
-        System.out.print(fibo1 + " ");
-        for(i=2;i<=numero;i++){
-             System.out.print(fibo2 + " ");
-             fibo2 = fibo1 + fibo2;
-             fibo1 = fibo2 - fibo1;
-        }
-        System.out.println();
+       
+        System.out.println("F(" + numero + ")=" + FibonacciRecursivo(numero-1));
     }
     
 }
